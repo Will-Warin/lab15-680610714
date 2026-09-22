1720575965a143f55770915ca8ec74f31b903c44
Will-Warin <warintorn_sr@cmu.ac.th>
2026-09-22 23:19:36 +0700

    calendar setting fix

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { BookOpen, Calendar, Home, Settings } from "lucide-react";
+import { BookOpen,  Home } from "lucide-react"; //Calendar,, Settings //เอา2ตัวนี้ออก
 import { Link, useLocation } from "react-router";
 
 import {
